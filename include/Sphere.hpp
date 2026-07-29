9fb1498fa756e6c88cd5ae96a139d07d532a91a6
Nihad <nihad.nemetli@gmail.com>
2026-07-30 01:33:44 +0400

    add: Sphere.hpp methods added

---
+++ include/Sphere.hpp
@@ -0,0 +1,14 @@
+#include "glad/glad.h"
+#include <GLFW/glfw3.h>
+
+namespace Sphere {
+
+    class Sphere {
+        public:
+            Sphere();
+            ~Sphere();
+        
+        protected:
+            void draw();
+    };
+}
