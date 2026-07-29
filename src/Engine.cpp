9552ebb656c2ff4f50a7debd52d06ef05ed63451
Nihad <nihad.nemetli@gmail.com>
2026-07-30 01:20:44 +0400

    upd: OpenGL version changed

--- src/Engine.cpp
+++ src/Engine.cpp
@@ -48,8 +48,8 @@ namespace Engine {
     }
 
     void Application::setupWindowHints() {
-        glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
-        glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+        glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
+        glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 6);
         glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
         glfwWindowHint(GLFW_DECORATED, GLFW_TRUE);
         glfwWindowHint(GLFW_RESIZABLE, GLFW_TRUE);
