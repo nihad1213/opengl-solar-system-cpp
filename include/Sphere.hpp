49759ba5e101672a45a966f41799ff70c3e273de
Nihad <nihad.nemetli@gmail.com>
2026-07-30 02:16:20 +0400

    add: Sphere part completed

--- include/Sphere.hpp
+++ include/Sphere.hpp
@@ -6,12 +6,21 @@
 
 namespace Sphere {
 
+    struct Vertex {
+        float x, y, z;
+    };
+
     class Sphere {
         public:
-            Sphere();
+            Sphere(float radius = 1.0f, unsigned int stacks = 20, unsigned int sectors = 20);
             ~Sphere();
-        
-        protected:
+
             void draw();
+        
+        private:
+            unsigned int VAO, VBO, EBO;
+            unsigned int indexCount;
+
+            void drawSphere(float radius, unsigned int stacks, unsigned int sectors);
     };
 }
--- src/Sphere.cpp
+++ src/Sphere.cpp
@@ -1,14 +1,88 @@
 #include "Sphere.hpp"
+#include <cmath>
 
 namespace Sphere {
 
-    Sphere::Sphere() {
+    Sphere::Sphere(float radius, unsigned int stacks, unsigned int sectors)
+    : VAO(0), VBO(0), EBO(0), indexCount(0)
+    {
+        drawSphere(radius, stacks, sectors);
     }
 
     Sphere::~Sphere() {
+        if (VAO != 0) glDeleteVertexArrays(1, &VAO);
+        if (VBO != 0) glDeleteBuffers(1, &VBO);
+        if (EBO != 0) glDeleteBuffers(1, &EBO);
+    }
+
+    void Sphere::drawSphere(float radius, unsigned int stacks, unsigned int sectors) {
+            const float PI = 3.14159265359f;
+            std::vector<Vertex> vertices;
+            std::vector<unsigned int> indices;
+
+            // Up to Above Stacks
+            for (unsigned int i = 0; i <= stacks; ++i) {
+                float stackAngle = PI / 2.0f - (float)i / stacks * PI;
+
+                float xy = radius * std::cos(stackAngle);
+                float y  = radius * std::sin(stackAngle);
+
+                // Left to Right Sectors
+                for (unsigned int j = 0; j <= sectors; ++j) {
+                    float sectorAngle = (float)j / sectors * 2.0f * PI;
+
+                    Vertex v;
+                    v.x = xy * std::cos(sectorAngle);
+                    v.y = y;
+                    v.z = xy * std::sin(sectorAngle);
+
+                    vertices.push_back(v);
+                }
+            }
+
+            for (unsigned int i = 0; i < stacks; ++i) {
+                unsigned int k1 = i * (sectors + 1);
+                unsigned int k2 = k1 + sectors + 1;
+
+                for (unsigned int j = 0; j < sectors; ++j, ++k1, ++k2) {
+                    if (i != 0) {
+                        indices.push_back(k1);
+                        indices.push_back(k2);
+                        indices.push_back(k1 + 1);
+                    }
+
+                    if (i != (stacks - 1)) {
+                        indices.push_back(k1 + 1);
+                        indices.push_back(k2);
+                        indices.push_back(k2 + 1);
+                    }
+                }
+            }
+
+            indexCount = static_cast<unsigned int>(indices.size());
+
+            glGenVertexArrays(1, &VAO);
+            glGenBuffers(1, &VBO);
+            glGenBuffers(1, &EBO);
+
+            glBindVertexArray(VAO);
+
+            glBindBuffer(GL_ARRAY_BUFFER, VBO);
+            glBufferData(GL_ARRAY_BUFFER, vertices.size() * sizeof(Vertex), vertices.data(), GL_STATIC_DRAW);
+
+            glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+            glBufferData(GL_ELEMENT_ARRAY_BUFFER, indices.size() * sizeof(unsigned int), indices.data(), GL_STATIC_DRAW);
+
+            glEnableVertexAttribArray(0);
+            glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, sizeof(Vertex), (void*)0);
+
+            glBindVertexArray(0);
+
     }
 
     void Sphere::draw() {
-        
+        glBindVertexArray(VAO);
+        glDrawElements(GL_TRIANGLES, indexCount, GL_UNSIGNED_INT, 0);
+        glBindVertexArray(0);
     }
 }
