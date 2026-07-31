af06c7d4910d7597175882ab4ae6cda9f9cdf36d
Nihad <nihad.nemetli@gmail.com>
2026-08-01 02:13:55 +0400

    upd: Camera logic changed, glm will be used

--- include/Camera.hpp
+++ include/Camera.hpp
@@ -2,19 +2,22 @@
 
 #include "glad/glad.h"
 #include <GLFW/glfw3.h>
-#include <cmath>
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
 
 namespace Camera {
 
     class Camera {
         public:
-            Camera();
-            ~Camera();
+            glm::vec3 position;
+            glm::vec3 front;
+            glm::vec3 up;
+            glm::vec3 right;
+            glm::vec3 worldUp;
 
+            float yaw;
+            float pitch;
         private:
-            void goForward();
-            void goBackward();
-            void goLeft();
-            void goRight();
+
     };
 }
