7520d3731830d755aba78f05b81cd587c8ffea65
Nihad <nihad.nemetli@gmail.com>
2026-09-04 00:19:29 +0400

    add: Planet.cpp functions implemented

--- src/Planet.cpp
+++ src/Planet.cpp
@@ -1,8 +1,29 @@
 #include "Planet.hpp"
-#include <iostream>
+#include "glad/glad.h"
 
 namespace Planet {
-    
-    
-}
 
+    Planet::Planet(float radius, unsigned int stacks, unsigned int sectors,
+                   float orbitDistance, bool isSun)
+        : sphere(radius, stacks, sectors), orbitDistance(orbitDistance), sun(isSun)
+    {}
+
+    Planet::~Planet() {}
+
+    void Planet::draw() {
+        glPushMatrix();
+        if (!sun) {
+            glTranslatef(orbitDistance, 0.0f, 0.0f);
+        }
+        sphere.draw();
+        glPopMatrix();
+    }
+
+    bool Planet::isSun() const {
+        return sun;
+    }
+
+    float Planet::getOrbitDistance() const {
+        return orbitDistance;
+    }
+}
